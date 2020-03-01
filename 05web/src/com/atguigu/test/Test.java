5e332d0d5d39e3e60c8619107f6fe36c9e111d04
Hushunkang <2752962035@qq.com>
2020-03-02 01:10:21 +0800

    XML&Tomcat&非maven工程，jar包也给你提交上去，这样工程才是完整滴

---
+++ 05web/src/com/atguigu/test/Test.java
@@ -0,0 +1,13 @@
+package com.atguigu.test;
+
+import org.jetbrains.annotations.Contract;
+
+public class Test {
+
+    @Contract(pure = true)
+    public static void main(String[] args) {
+
+
+    }
+
+}
