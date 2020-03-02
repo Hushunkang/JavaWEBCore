84408a7259e344489252512ba414e790ae02eade
Hushunkang <2752962035@qq.com>
2020-03-03 07:04:30 +0800

    使用注解@WebServlet来配置Servlet

---
+++ 06webtest/src/com/atguigu/servlet/MyServlet.java
@@ -0,0 +1,25 @@
+package com.atguigu.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author hskBeginner Email：2752962035@qq.com
+ * @version 1.0
+ * @description
+ * @create 2020年03月03日 06时51分39秒
+ */
+@WebServlet(name = "MyServlet",urlPatterns = "/MyServlet")
+public class MyServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("doPost");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("doGet");
+    }
+}
