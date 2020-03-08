f7b9be7dc70cd32a3eda8be3d9db6a138c0bb26e
Hushunkang <2752962035@qq.com>
2020-03-09 05:28:03 +0800

    file upload---文件上传操作

---
+++ 09el_jstl/src/com/atguigu/servlet/UploadServlet.java
@@ -0,0 +1,73 @@
+package com.atguigu.servlet;
+
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class UploadServlet extends HttpServlet {
+    /**
+     * 用来处理上传的数据
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        System.out.println("文件上传的请求来了...");
+        //commons-io-1.4.jar commons-fileupload-1.2.1.jar apache提供的文件上传功能的jar
+        //以多段的形式（每一个数据段对应着一个表单项）进行拼接，然后将数据以二进制流形式提交给服务器
+        //那么此时需要注意，服务端接收数据的时候也必须要以流的形式接收，用req.getParameter这种方式是不正确的
+//        ServletInputStream inputStream = req.getInputStream();
+//        byte[] bbuff = new byte[10240000];//给字节缓冲区设置大一点，可以一次性让所有数据读到字节缓冲区，而不需要多次读
+//        int read = inputStream.read(bbuff);
+//        System.out.println(new String(bbuff,0,read));
+
+        //1 先判断上传的数据是否多段数据（只有是多段的数据，才是文件上传的）
+        if (ServletFileUpload.isMultipartContent(req)) {
+//           创建FileItemFactory工厂实现类
+            FileItemFactory fileItemFactory = new DiskFileItemFactory();
+            // 创建用于解析上传数据的工具类ServletFileUpload类
+            ServletFileUpload servletFileUpload = new ServletFileUpload(fileItemFactory);
+            try {
+                // 解析上传的数据，得到每一个表单项FileItem
+                List<FileItem> list = servletFileUpload.parseRequest(req);
+                // 循环判断，每一个表单项，是普通类型，还是上传的文件
+                for (FileItem fileItem : list) {
+
+                    if (fileItem.isFormField()) {
+                        // 普通表单项
+
+                        System.out.println("表单项的name属性值：" + fileItem.getFieldName());
+                        // 参数UTF-8.解决乱码问题
+                        System.out.println("表单项的value属性值：" + fileItem.getString("UTF-8"));
+
+                    } else {
+                        // 上传的文件
+                        System.out.println("表单项的name属性值：" + fileItem.getFieldName());
+                        System.out.println("上传的文件名：" + fileItem.getName());
+
+//                        fileItem.write(new File("e:\\" + fileItem.getName()));
+                        fileItem.write(new File("e:" + File.separator + fileItem.getName()));
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+}
