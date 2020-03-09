55d8e1b2802c082b72df6dc4a23eca4a78881f9d
Hushunkang <2752962035@qq.com>
2020-03-09 09:00:35 +0800

    file download---文件下载操作

---
+++ 09el_jstl/src/com/atguigu/servlet/Download.java
@@ -0,0 +1,58 @@
+package com.atguigu.servlet;
+
+import org.apache.commons.io.IOUtils;
+import sun.misc.BASE64Encoder;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URLEncoder;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Download extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        1、获取要下载的文件名
+        String downloadFileName = "2.jpg";
+//        2、读取要下载的文件内容 (通过ServletContext对象可以读取)
+        ServletContext servletContext = getServletContext();
+        // 获取要下载的文件类型
+        String mimeType = servletContext.getMimeType("/file/" + downloadFileName);
+        System.out.println("下载的文件类型：" + mimeType);
+//        4、在回传前，通过响应头告诉客户端返回的数据类型
+        resp.setContentType(mimeType);
+//        5、还要告诉客户端收到的数据是用于下载使用（还是使用响应头）
+        // Content-Disposition响应头，表示收到的数据怎么处理
+        // attachment表示附件，表示下载使用，而不是给资源直接给展示在浏览器上面
+        // filename= 表示指定下载的文件名
+        // http协议设计之初没考虑到中文，返回数据中有中文默认就乱码了
+        // url编码是把汉字转换成为%xx%xx的格式
+        if (req.getHeader("User-Agent").contains("Firefox")) {
+            // 如果是火狐浏览器使用Base64编码
+            resp.setHeader("Content-Disposition", "attachment; filename==?UTF-8?B?" + new BASE64Encoder().encode("中国.jpg".getBytes("UTF-8")) + "?=");
+        } else {
+            // 如果不是火狐，是IE或谷歌，使用URL编码操作
+            resp.setHeader("Content-Disposition", "attachment; filename=" + URLEncoder.encode("中国.jpg", "UTF-8"));
+        }
+        /**
+         * /斜杠被服务器解析表示地址为http://ip:prot/工程名/  映射 到代码的Web目录
+         */
+//        InputStream resourceAsStream = servletContext.getResourceAsStream("/file/" + downloadFileName);//目前没找出来是什么原因，代码看似没问题但获取的到的流居然为null
+        Path path = Paths.get("E:\\ideaWorkspace01\\JavaWEBCore\\09el_jstl\\web\\file\\2.jpg");
+        InputStream resourceAsStream = Files.newInputStream(path);
+        // 获取响应的输出流
+        OutputStream outputStream = resp.getOutputStream();
+        //        3、把下载的文件内容回传给客户端
+        // 读取输入流中全部的数据，复制给输出流，输出给客户端
+        IOUtils.copy(resourceAsStream, outputStream);
+    }
+}
