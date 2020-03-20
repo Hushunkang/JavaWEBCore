a70c07c0a7fbf2165e2a91c2f13074e2f54ba4ab
Hushunkang <2752962035@qq.com>
2020-03-20 11:24:36 +0800

    使用Filter来统一给所有的Service方法都加上try-catch来管理事务

---
+++ book/src/com/atguigu/filter/TransactionFilter.java
@@ -0,0 +1,48 @@
+package com.atguigu.filter;
+
+import com.atguigu.utils.JdbcUtils;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * @author hskBeginner Email：2752962035@qq.com
+ * @version 1.0
+ * @description
+ * @create 2020年03月20日 10时52分01秒
+ */
+public class TransactionFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    /**
+     * 使用Filter来统一给所有的Service方法都加上try-catch来管理事务
+     * @param request
+     * @param response
+     * @param chain
+     * @throws IOException
+     * @throws ServletException
+     */
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        try {
+            chain.doFilter(request,response);//放行，执行本次请求的下一个filter，
+            // 承接上文。。。若没有则请求目标资源，若请求的是Servlet，
+            // 承接上文。。。此处则是根据请求类型调用了BaseServlet类中的doGet或者doPost方法
+            JdbcUtils.commitAndClose();// 提交事务
+        } catch (Exception e) {
+            JdbcUtils.rollbackAndClose();//回滚事务
+            e.printStackTrace();
+            throw new RuntimeException(e);//把异常抛给Tomcat管理展示友好的错误页面
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+}
--- book/src/com/atguigu/web/BaseServlet.java
+++ book/src/com/atguigu/web/BaseServlet.java
@@ -25,6 +25,9 @@ public abstract class BaseServlet extends HttpServlet {
             method.invoke(this, req, resp);
         } catch (Exception e) {
             e.printStackTrace();
+            throw new RuntimeException(e);//将异常抛给这个方法的调用方法，此处即为Filter
+            //承接上文。。。这里必须手动的抛出异常对象，让调用本方法的方法知道有异常了，
+            //承接上文。。。而不能单纯的用try-catch给异常吃掉了！！！
         }
     }
 
